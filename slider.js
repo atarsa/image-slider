08041ca74d2136334b2a5831761a28e255cb0c0b
ATarsa <agnieszkatarsa@gmail.com>
2019-10-01 20:12:28 +0100

    Add circles indecation functionality

--- slider.js
+++ slider.js
@@ -1,17 +1,18 @@
-const slides = document.getElementsByClassName("slide");
+const slides = document.getElementsByClassName('slide');
+const cirlces = document.getElementsByClassName('circle');
 let currentSlide = 0;
 
 
 function showSlide(slideNb) {
   // hide all other images
   Array.from(slides).forEach( slide => {
-    slide.style.display = "none";
+    slide.style.display = 'none';
     slide.style.opacity = 0;
     slide.classList.remove('transition');
   })
 
   // show only current slide
-  slides[slideNb].style.display = "block";
+  slides[slideNb].style.display = 'block';
   slides[slideNb].classList.add('transition');
   window.setTimeout( function(){
     slides[slideNb].style.opacity = 1;
@@ -40,20 +41,50 @@ function prevSlide(currentSlide){
   }
 }
 
+function updateCircleColour(cirlces, circleNb){
+  // clear background colour of all elements
+  Array.from(cirlces).forEach(circle => {
+    circle.style.background = 'none';
+  })
+  // change background colour of the next circle  to grey
+  cirlces[circleNb].style.background = 'grey'
+}
 
-
-// add event listeners for arrows press
+// add event listeners 
+//for arrows press
 document.querySelector('.fa-chevron-right')
-  .addEventListener("click", function(){
+  .addEventListener('click', function(){
     currentSlide = nextSlide(currentSlide);
     showSlide(currentSlide);
+    // update circles indecator
+    updateCircleColour(cirlces, currentSlide);
 })
 
 document.querySelector('.fa-chevron-left')
-  .addEventListener("click", function(){
+  .addEventListener('click', function(){
     currentSlide = prevSlide(currentSlide);
     showSlide(currentSlide);
+    // update circles indecator
+    updateCircleColour(cirlces, currentSlide);
 })
 
+// for circle press
+for (let i =0; i < cirlces.length; i++){
+  cirlces[i].addEventListener('click', function(e){
+    // clear background colour of all elements
+    Array.from(cirlces).forEach(circle => {
+      circle.style.background = 'none';
+    })
+    // change background colour of the clicked element to grey
+    e.target.style.background = 'grey';
+    // update current slide
+    let innerSlide = parseInt(e.target.dataset.id);
+    currentSlide = innerSlide;
+    // show current slide
+    showSlide(innerSlide);
+  })
+}
 
+  
 showSlide(0);
+updateCircleColour(cirlces, 0);
